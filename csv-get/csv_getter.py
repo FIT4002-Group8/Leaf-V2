46e1bab0d066d36ff8427932a1551cb7486c239d
czub0002 <czub0002@student.monash.edu>
2024-07-26 21:13:36 +1000

    Converted to Flask app and created a test file.

---
+++ csv-get/csv_getter.py
@@ -0,0 +1,58 @@
+import os
+from flask import Flask, send_file, jsonify, request
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import pyzipper
+import tempfile
+
+app = Flask(__name__)
+
+
+def authenticate_drive():
+    gauth = GoogleAuth()
+    gauth.LoadClientConfigFile("client_secret.json")
+    gauth.LoadCredentialsFile("mycreds.txt")
+
+    if gauth.credentials is None:
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
+
+    gauth.SaveCredentialsFile("mycreds.txt")
+    return GoogleDrive(gauth)
+
+
+@app.route('/download-zip', methods=['GET'])
+def download_zip():
+    file_id = request.args.get('file_id')
+    if not file_id:
+        return jsonify({"error": "file_id parameter is required"}), 400
+
+    try:
+        drive = authenticate_drive()
+
+        csv_file_name = 'downloaded_file.csv'
+        zip_file_name = 'protected_file.zip'
+        password = 'password'
+
+        temp_csv_path = os.path.join(tempfile.gettempdir(), csv_file_name)
+        downloaded_file = drive.CreateFile({'id': file_id})
+        downloaded_file.GetContentFile(temp_csv_path)
+
+        zip_file_path = os.path.join(tempfile.gettempdir(), zip_file_name)
+        with pyzipper.AESZipFile(zip_file_path, 'w', compression=pyzipper.ZIP_DEFLATED,
+                                 encryption=pyzipper.WZ_AES) as zf:
+            zf.setpassword(password.encode())
+            zf.write(temp_csv_path, csv_file_name)
+
+        os.remove(temp_csv_path)
+        return send_file(zip_file_path, as_attachment=True, download_name=zip_file_name)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
